64ab71295e1f0e936c94794589065413bf9a915a
bopboy <scp0000@yahoo.com>
2024-01-05 16:18:26 +0900

    http 요청 클래스

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
 <?php
 
-echo "test\n";
 require_once './vendor/autoload.php';
-require_once './config.php';
+// require_once './config.php';
 
-use Onedb\Database\Adaptor;
+// use Onedb\Database\Adaptor;
 
-Adaptor::setup('mysql:dbname='.$config['dbname'],$config['dbuser'],$config['password']);
+// Adaptor::setup('mysql:dbname='.$config['dbname'],$config['dbuser'],$config['password']);
 
-class Post {
+// class Post {
 
-}
+// }
 
-$posts1 = Adaptor::getAll('SELECT * FROM lectures LIMIT 3');
-$posts2 = Adaptor::getAll('SELECT * FROM lectures LIMIT 10', [], Post::class);
-var_dump($posts2);
+// $posts1 = Adaptor::getAll('SELECT * FROM lectures LIMIT 3');
+// $posts2 = Adaptor::getAll('SELECT * FROM lectures LIMIT 10', [], Post::class);
+// var_dump($posts2);
+
+echo "test\n";
+use Onedb\Http\Request;
+
+$_SERVER['REQUEST_METHOD'] = 'GET';
+$_SERVER['PATH_INFO'] = '/posts/write';
+var_dump(Request::getMethod());
+var_dump(Request::getPath());
---
+++ src/Http/Request.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Onedb\Http;
+
+class Request {
+    public static function getMethod() {
+        return filter_input(INPUT_POST, '_method') ?: $_SERVER['REQUEST_METHOD'];
+    }
+    public static function getPath() {
+        return $_SERVER['PATH_INFO'] ?? '/';
+    }
+}
